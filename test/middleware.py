99c5b7ebb17ab2b4cd98290886f373d6a95814e1
Dusheba <dusheba.olga@gmail.com>
2021-05-31 01:27:27 +0300

    creating models for workold

--- test/middleware.py
+++
@@ -1,8 +0,0 @@
-#
-# from django.http import HttpResponse, response
-#
-#
-# def post_error(request):
-#     # if response = request.POST:
-#     #if request.method == "POST":
-#         return HttpResponse('ERR')
